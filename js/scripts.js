9d7769fcc093acfaa5bd29d2942c420bbce5d855
mogonchi <monyenyedolvine@gmail.com>
2022-03-12 17:44:30 +0300

    add index.html changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,85 +1,87 @@
-$(document).ready(function(){
-    $(".design-image").click(function(){
-        $(".design-image").fadeIn("slow","linear").hide();
+$(document).ready(function () {
+    $(".design-image").click(function () {
+        $(".design-image").fadeIn("slow", "linear").hide();
         $("#design").show();
     });
-    $("#design").click(function(){
+    $("#design").click(function () {
         $("#design").fadeOut("swing");
-        $(".design-image").fadeIn("slow","swing");
+        $(".design-image").fadeIn("slow", "swing");
     })
 });
 
-$(document).ready(function(){
-    $(".dev-image").click(function(){
-        $(".dev-image").fadeIn("slow","linear").hide();
+$(document).ready(function () {
+    $(".dev-image").click(function () {
+        $(".dev-image").fadeIn("slow", "linear").hide();
         $("#development").show();
     });
-    $("#development").click(function(){
+    $("#development").click(function () {
         $("#development").fadeOut("swing");
-        $(".dev-image").fadeIn("slow","swing");
+        $(".dev-image").fadeIn("slow", "swing");
     });
 });
-$(document).ready(function(){
-    $(".product-image").click(function(){
-        $(".product-image").fadeIn("slow","linear").hide();
+$(document).ready(function () {
+    $(".product-image").click(function () {
+        $(".product-image").fadeIn("slow", "linear").hide();
         $("#management").show();
     });
-    $("#management").click(function(){
+    $("#management").click(function () {
         $("#management").fadeOut("swing");
-        $(".product-image").fadeIn("slow","swing");
+        $(".product-image").fadeIn("slow", "swing");
     });
 });
 
-// $(document).ready(function(){
-//     $(("#work4, textHover1").animate({
-//         opacity:1
-//     });
-//     $("#work4, textHover1").hover(function(){
-//         $(this).stop().animate({opacit:.4},200);
-//     }, function(){
-//         $(this).stop().animate({opacity:1},500)
-//     });
-// }); 
- 
-$(document).ready(function(){
-    $("#work4").mouseover(function(){
+$(document).ready(function () {
+    $("#work4").mouseover(function () {
         $("#textHover1").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
         $("#textHover1").hide();
     });
-    $("#work3").mouseover(function(){
+    $("#work3").mouseover(function () {
         $("#textHover2").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
         $("#textHover2").hide();
     });
-    $("#work2").mouseover(function(){
+    $("#work2").mouseover(function () {
         $("#textHover3").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
         $("#textHover3").hide();
     });
-    $("#work1").mouseover(function(){
+    $("#work1").mouseover(function () {
         $("#textHover4").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
         $("#textHover4").hide();
     });
-    $("#work5").mouseover(function(){
+    $("#work5").mouseover(function () {
         $("#textHover5").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
         $("#textHover5").hide();
     });
-    $("#work6").mouseover(function(){
+    $("#work6").mouseover(function () {
         $("#textHover6").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
         $("#textHover6").hide();
     });
-    $("#work7").mouseover(function(){
+    $("#work7").mouseover(function () {
         $("#textHover7").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
         $("#textHover7").hide();
     });
-    $("#work8").mouseover(function(){
+    $("#work8").mouseover(function () {
         $("#textHover8").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
         $("#textHover8").hide();
+    });
+});
+$(document).ready(function () {
+    $("#form-submit").submit(function () {
+        let name = $("input#name").val();
+        let email = $("input#email").val();
+        let comments = $("input#comments").val();
+
+        if ($("input#name").val() && $("input#email").val()) {
+            alert(name + "we have received your message. Thank you for reaching out to us.");
+        }else {
+            alert("Please enter your name and email")
+        }
     });
 });
